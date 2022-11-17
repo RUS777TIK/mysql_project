f171e09f4495b088465c20714a23a7fd57ca2974
bgu-user <rustam.sadukov.777@yandex.ri>
2022-11-17 08:43:01 +0500

    ssssssss

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from configparser import ConfigParser
+import sys
 
-config = ConfigParser()
-config.read('./config.default.ini')
+from dynaconf import Dynaconf
+from packages.utils import Logger
+from packages.abstract import Connection
+from packages.services import *
+from packages.services import __meta__ as service_meta
 
-db_host = config['MySQL']['host']
-db_port = int(config['MySQL']['port'])
-db_user = config['MySQL']['user']
-db_pass = config['MySQL']['password']
+
+class Application:
+    def __init__(self, config_file):
+        config = Dynaconf(
+            settings_file=[config_file],
+            environments=False
+        )
+        Logger.config(config.get('Logger'))
+        try:
+            Connection.connect(config.get('MySQL', dict()))
+            Logger.info('Соединение с базой данных выполнено')
+            for model in service_meta:
+                model.metadata.create_all(Connection.get_instance())
+            Logger.info('Все модели созданы')
+        except Exception as e:
+            Logger.exception(e)
+            raise e
+    def run(self):
+        Logger.info('Запуск приложения')
+
+
+if __name__ == '__main__':
+    try:
+        app = Application('./config.json')
+        app.run()
+    except:
+        sys.exit()
---
+++ packages/utils/logger.py
@@ -0,0 +1,51 @@
+from loguru import logger
+from pathlib import PurePath
+import sys
+
+
+class Logger:
+    __config = {
+        'backtrace': True,
+        'diagnose': True,
+        'format': '{time} [{level}] {message}'
+    }
+    debug = logger.debug
+    info = logger.info
+    warning = logger.warning
+    error = logger.error
+    exception = logger.opt(exception=True).critical
+
+    @staticmethod
+    def config(config=None):
+        if not config:
+            return
+        console = config.get('console')
+        file = config.get('file', dict())
+        if console:
+            logger.remove()
+            console_config = Logger.__config.copy()
+            _format = console.get('format')
+            if _format:
+                console_config['format'] = _format
+            console_config['colorize'] = True
+            logger.add(sys.stdout, **console_config, level='DEBUG')
+        if file:
+            path = file.get('path')
+            if not path:
+                return
+            console_config = Logger.__config.copy()
+            _format = file.get('format')
+            if _format:
+                console_config['format'] = _format
+            console_config['rotation'] = file.get('rotation')
+            logger.add(
+                PurePath(path, 'app.log'),
+                **console_config,
+                filter=lambda record: record['level'].name in ['DEBUG', 'INFO', 'WARNING']
+            )
+            logger.add(
+                PurePath(path, 'error.log'),
+                **console_config,
+                filter=lambda record: record['level'].name in ['ERROR', 'CRITICAL']
+            )
+
